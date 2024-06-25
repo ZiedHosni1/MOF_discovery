0373fca428620c7bc6773aa43f2d20733fc86e4e
Darkdoggg <zczqzni@ucl.ac.uk>
2024-06-25 16:11:51 +0800

    create a folder

---
+++ try.py
@@ -0,0 +1,8 @@
+import os
+
+# Given file path
+file_path = 'C:\\Users\\juice\\PycharmProjects\\MOF_discovery_Zyiao_2\\MOF.mol2'
+
+# Get the directory of the file
+directory_path = os.path.dirname(file_path)
+print(f"Directory of the file: {directory_path}")
