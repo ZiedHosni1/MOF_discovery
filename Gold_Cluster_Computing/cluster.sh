b78c59e465231d33b61d8b6e9b62bc878fbabd39
Ziyao Ning <152184609+Darkdoggg@users.noreply.github.com>
2024-07-03 00:31:04 +0800

    Delete Gold_Cluster_Computing directory

--- Gold_Cluster_Computing/cluster.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-module purge
-# If required, uncomment the lines below and adjust for your system
-# module load singularity
-# module load python3
-
-singularity exec --bind ${SHARED_FILESYSTEM},${GOLDHPC_DIR} ${GOLDHPC_DIR}/${GOLDIMAGE} python3 ${GOLDHPC_DIR}/cluster_docking.py "${CCDC_LICENSING_CONFIGURATION}" "${OUTPUT_DIR}" "${GOLD_LOG_LEVEL}"
