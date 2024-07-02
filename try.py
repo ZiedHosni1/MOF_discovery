ad069795c5547a69f734e6bb06e08b5811f0b414
Ziyao Ning <152184609+Darkdoggg@users.noreply.github.com>
2024-07-03 00:33:17 +0800

    Delete try.py

--- try.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Given file path
-file_path = 'C:\\Users\\juice\\PycharmProjects\\MOF_discovery_Zyiao_2\\MOF.mol2'
-
-# Get the directory of the file
-directory_path = os.path.dirname(file_path)
-print(f"Directory of the file: {directory_path}")
